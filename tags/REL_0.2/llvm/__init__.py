65bb1fb9b93b39d8357b8b00c7258ef56ac6a929
mdevan.foobar <mdevan.foobar@8d1e9007-1d4e-0410-b67e-1979fd6579aa>
2008-06-15 04:03:00 +0000

    Tagging 0.2 release.

---
+++ tags/REL_0.2/llvm/__init__.py
@@ -0,0 +1,41 @@
+"""Common classes related to LLVM.
+
+"""
+
+VERSION = '0.2'
+
+#===----------------------------------------------------------------------===
+# Exceptions
+#===----------------------------------------------------------------------===
+
+class LLVMException(Exception):
+
+    def __init__(self, msg=""):
+        Exception.__init__(self, msg)
+
+
+#===----------------------------------------------------------------------===
+# Ownables
+#===----------------------------------------------------------------------===
+
+class Ownable(object):
+
+    def __init__(self, ptr, del_fn):
+        self.ptr = ptr
+        self.owner = None
+        self.del_fn = del_fn
+
+    def _own(self, owner):
+        if self.owner:
+            raise LLVMException, "object already owned"
+        self.owner = owner
+
+    def _disown(self):
+        if not self.owner:
+            raise LLVMException, "not owned"
+        self.owner = None
+
+    def __del__(self):
+        if not self.owner:
+            self.del_fn(self.ptr)
+
